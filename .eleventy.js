4096c5a2fba36dcabd619fec1b4570b4d039e688
Frank <6370230+FrankDraws@users.noreply.github.com>
2023-11-25 11:02:23 -0500

    Revised css, added year formatting to eleventy.js.

--- .eleventy.js
+++ .eleventy.js
@@ -11,13 +11,21 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
+    
+    eleventyConfig.addFilter("currentYear", () => {
+        return DateTime.local().year;
+    });
     // end date formatting
+    
 
     return {
         dir: {
             input: "src",
             output: "public",
         },
+        // Make sure the following actually work.
+        templateFormats: ["html", "njk", "md"],
+        passthroughFileCopy: true,
     };
 };
 
